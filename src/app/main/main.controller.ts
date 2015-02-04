1c772605e44c9c33301997136577a0551984c04a
James Trinklein <james.trinklein@changepoint.com>
2015-02-04 02:30:09 -0800

    move Match class to own file. clean up less

--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -1,72 +1,7 @@
 /// <reference path='./Team/Team.ts' />
 
 module tourneyTracker {
-    export class Match {
-        public teamA: Team;
-        public teamB: Team;
-        public winner: Team;
-        public loser: Team;
-        public parent: Match;
-        public leftChild: Match;
-        public rightChild: Match;
 
-        constructor(teamA?: Team, teamB?: Team) {
-            this.teamA = teamA;
-            this.teamB = teamB;
-
-        }
-
-        private setWinner(team: Team): void {
-            this.winner = team;
-            if (this.parent) {
-                this.parent.updateTeams();
-            }
-        }
-
-        public teamAWon(): void {
-            this.setWinner(this.teamA);
-        }
-
-        public teamBWon(): void {
-            this.setWinner(this.teamB);
-        }
-
-        public updateTeams(): void {
-            if (this.leftChild) {
-                this.teamA = this.leftChild.winner;
-            }
-            if (this.rightChild) {
-                this.teamB = this.rightChild.winner;
-            }
-            if(this.parent) {
-                this.parent.updateTeams();
-            }
-        }
-
-        private getTeamName(team: Team): string {
-            return !!team ? team.name : '???';
-        }
-        public getTeamNameA(): string {
-            return this.getTeamName(this.teamA);
-        }
-
-
-        public getTeamNameB(): string {
-            return this.getTeamName(this.teamB);
-        }
-
-        public setLeftChild(match: Match): void {
-            match.parent = this;
-            this.leftChild = match;
-            this.teamA = match.winner;
-        }
-
-        public setRightChild(match: Match): void {
-            match.parent = this;
-            this.rightChild = match;
-            this.teamB = match.winner;
-        }
-    }
     export interface IMainScope extends ng.IScope {
         teams: Array<Team>;
         addTeam(name: string);
@@ -150,6 +85,7 @@ module tourneyTracker {
                 m.teamB = teams[i++];
             });
         }
+        
         private createLevelLists(rootMatch: Match): Array<Array<Match>> {
             var list: Array<Array<Match>> = [];
             var currentList: Array<Match> = [rootMatch];
