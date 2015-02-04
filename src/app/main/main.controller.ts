a7c97db520846b7699a05e7ac205e58744df77a5
James Trinklein <james.trinklein@changepoint.com>
2015-02-04 00:40:06 -0800

    build match tree from root up, calculate spacing for matches by level

--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -46,6 +46,7 @@ module tourneyTracker {
         matches: Array<Match>;
         createMatchTree(): void;
         levels: Array<Array<Match>>;
+        getStyleForLevel(lvl: number): Object;
     }
 
     export class MainCtrl {
@@ -56,50 +57,82 @@ module tourneyTracker {
             $scope.matches = new Array<Match>();
             $scope.addTeam = (name: string) => { _this.addTeam(name); };
             $scope.createMatchTree = () => { _this.createMatchTree(); };
+            $scope.getStyleForLevel = (n: number): Object => { return _this.getStyleForLevel(n); };
+            this.addTeam('team1');
+            this.addTeam('team2');
+            this.addTeam('team3');
+            this.addTeam('team4');
+            this.addTeam('team5');
+            this.addTeam('team6');
+            this.addTeam('team7');
+            this.addTeam('team8');
+            this.addTeam('team9');
+            this.createMatchTree();
         }
-
-        public generateBaseMatches(): void {
-            var last: Team = null;
-            var matches: Array<Match> = [];
-            this.$scope.teams.reduce((match: Match, team: Team): Match =>  {
-                if (!match.teamA) {
-                    match.teamA = team;
-                } else {
-                    match.teamB = team;
-                    matches.push(match);
-                    match = new Match();
-                }
-                return match;
-            }, new Match());
-
-            this.$scope.matches = matches;
+        private calculateMarginTop(level: number): string {
+            return ((Math.pow(2, level) - 1) * 40) + 'px';
+        }
+        private calculateMarginBottom(level: number): string {
+            return (Math.pow(2, level + 1) * 40 - 60) + 'px';
+        }
+        public getStyleForLevel(level: number): Object {
+            return {
+                'margin-top': this.calculateMarginTop(level),
+                'margin-bottom': this.calculateMarginBottom(level)
+            };
         }
 
         public createMatchTree(): void {
-            var levels: Array<Array<Match>> = [];
-            var childNodes: Array<Match> = this.$scope.matches;
-
-            while (childNodes.length > 0) {
-                levels.push(childNodes);
-                var treeNodes: Array<Match> = [];
-                childNodes.reduce((parent: Match, child: Match): Match => {
-                    if(!parent.leftChild) {
-                        parent.leftChild = child;
-                        return parent;
-                    } else {
-                        parent.rightChild = child;
-                        treeNodes.push(parent);
-                        return new Match();
-                    }
-                }, new Match());
-                childNodes = treeNodes;
+            var root: Match = new Match();
+            var requiredLeafNodes: number = Math.ceil(this.$scope.teams.length / 2);
+            console.log(requiredLeafNodes);
+            var openNodes: Array<Match> = [root];
+            var leafNodes: number = 1;
+            while(leafNodes < requiredLeafNodes) {
+                var node: Match = openNodes[0];
+                var newLeaf: Match = new Match();
+                openNodes.push(newLeaf);
+                if (!node.leftChild) {
+                    node.setLeftChild(newLeaf);
+                } else {
+                    node.setRightChild(newLeaf);
+                    openNodes.shift();
+                    leafNodes++;
+                }
             }
-            this.$scope.levels = levels;
+            var teams: Array<Team> = this.$scope.teams;
+            if (teams.length % 2 === 1) {
+                teams.push(new Team('BYE'));
+            }
+            var i: number = 0;
+            openNodes.forEach((m: Match): void => {
+                m.teamA = teams[i++];
+                m.teamB = teams[i++];
+            });
+
+            var list: Array<Array<Match>> = [];
+            var currentList: Array<Match> = [root];
+
+            while(currentList) {
+                list.unshift(currentList);
+                var next: Array<Match> = [];
+                currentList.forEach((m): void => {
+                    if (m.leftChild) {
+                        next.push(m.leftChild);
+                    }
+
+                    if (m.rightChild) {
+                        next.push(m.rightChild);
+                    }
+                });
+                currentList = (next.length > 0) ? next : null;
+            }
+
+            this.$scope.levels = list;
         }
 
         public addTeam(name: string): void {
             this.$scope.teams.push(new Team(name));
-            this.generateBaseMatches();
         }
     }
 }
